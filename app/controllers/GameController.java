94c4f1eccb53f6162cabd52f019082fd6a98992a
kellbyhudson <kellbyhudson@gmail.com>
2018-08-27 15:26:31 -0500

    Word Game HTML/Java (Gold)

--- app/controllers/GameController.java
+++ app/controllers/GameController.java
@@ -1,11 +1,26 @@
 package controllers;
 
 import models.Words;
+import play.data.DynamicForm;
+import play.data.FormFactory;
 import play.mvc.Controller;
 import play.mvc.Result;
 
+import javax.inject.Inject;
+import java.util.HashSet;
+import java.util.Set;
+
 public class GameController extends Controller
 {
+    private Set<Words> usedMadLib = new HashSet<>();
+    private FormFactory formFactory;
+
+    @Inject
+    public GameController(FormFactory formFactory)
+    {
+        this.formFactory = formFactory;
+    }
+
     public Result getStart()
     {
         return ok(views.html.start.render());
@@ -16,6 +31,73 @@ public class GameController extends Controller
         Words words = new Words();
         words.setPlace1("Parking Lot");
         words.setAdjective1("fuzzy");
+        words.setNoun1("truck");
+        words.setNoun2("squirrel");
+        words.setVerbPastTense1("infected");
+
+        return ok(views.html.story.render(words));
+    }
+
+    public Result getSecondStory()
+    {
+        Words words = new Words();
+        words.setPlace1("Louis' Camper Trailer");
+        words.setAdjective1("ruthless");
+        words.setNoun1("doll");
+        words.setNoun2("flame");
+        words.setVerbPastTense1("waxed");
+
+        return ok(views.html.story.render(words));
+    }
+
+    public Result getThirdStory()
+    {
+        Words words = new Words();
+        words.setPlace1("Corner Store");
+        words.setAdjective1("uppity");
+        words.setNoun1("hammer");
+        words.setNoun2("clover");
+        words.setVerbPastTense1("viewed");
+
+        return ok(views.html.story.render(words));
+    }
+
+    public Result getRandomStory()
+    {
+        Words words = new Words();
+        words.setRandomPlace1();
+        words.setRandomAdjective1();
+        words.setRandomNoun1();
+        words.setRandomNoun2();
+        words.setRandomVerbPastTense1();
+        if (!usedMadLib.add(words))
+        {
+            getRandomStory();
+        }
+        return ok(views.html.story.render(words));
+    }
+
+    public Result getMyOwnStory()
+    {
+        return ok(views.html.selectwords.render());
+    }
+
+    public Result postMyOwnStory()
+    {
+        DynamicForm form = formFactory.form().bindFromRequest();
+
+        String place1 = form.get("place1");
+        String adjective1 = form.get("adjective1");
+        String noun1 = form.get("noun1");
+        String noun2 = form.get("noun2");
+        String verb1 = form.get("verb1");
+
+        Words words = new Words();
+        words.setPlace1(place1);
+        words.setAdjective1(adjective1);
+        words.setNoun1(noun1);
+        words.setNoun2(noun2);
+        words.setVerbPastTense1(verb1);
 
         return ok(views.html.story.render(words));
     }
--- app/models/Words.java
+++ app/models/Words.java
@@ -1,9 +1,15 @@
 package models;
 
+import java.util.Random;
+
 public class Words
 {
+    private String imageUrl;
     private String place1;
     private String adjective1;
+    private String noun1;
+    private String noun2;
+    private String verbPastTense1;
 
     public String getPlace1()
     {
@@ -15,6 +21,30 @@ public class Words
         this.place1 = place1;
     }
 
+    public void setRandomPlace1()
+    {
+        Random random = new Random();
+        int number = random.nextInt(13);
+        String[] places = {"Walmart", "Pizza Ranch", "Grandma's House", "A Kitchen in Hell's Kitchen",
+                "The Looney Bin", "Dirty Bar Bathroom", "The Blue Lagoon", "Chachsky's", "Tilted Kilt of Conway",
+                "Toad Suck Campgrounds", "Drug Dealer's laundry room", "Arizona Waterfront Property", "Persia"};
+        this.place1 = places[number];
+        setImageUrl(number);
+    }
+
+    public String getImageUrl()
+    {
+        return imageUrl;
+    }
+
+    public void setImageUrl(int imageAssignment)
+    {
+        String[] images = {"/assets/images/walmart.jpg", "/assets/images/pizzaranch.jpg", "/assets/images/grandmashouse.jpg", "/assets/images/hellskitchen.jpg",
+                "/assets/images/looneybin.jpg", "/assets/images/barbathroom.jpg", "/assets/images/bluelagoon.jpg", "/assets/images/chachskys.jpg", "/assets/images/tiltedkilt.png",
+                "/assets/images/toadsuck.jpg", "/assets/images/drugdealersroom.jpg", "/assets/images/arizonawaterfront.jpg", "/assets/images/persia.jpg"};
+        this.imageUrl = images[imageAssignment];
+    }
+
     public String getAdjective1()
     {
         return adjective1;
@@ -24,4 +54,71 @@ public class Words
     {
         this.adjective1 = adjective1;
     }
+
+    public void setRandomAdjective1()
+    {
+        Random random = new Random();
+        int number = random.nextInt(14);
+        String[] places = {"nutty", "acidic", "loose", "splendid", "amusing", "little", "healthy",
+                "ludicrous", "ignorant", "ritzy", "needy", "vivacious", "clammy", "useless" };
+        this.adjective1 = places[number];
+    }
+
+    public String getNoun1()
+    {
+        return noun1;
+    }
+
+    public void setNoun1(String noun1)
+    {
+        this.noun1 = noun1;
+    }
+
+    public void setRandomNoun1()
+    {
+        Random random = new Random();
+        int number = random.nextInt(15);
+        String[] places = {"glue", "money", "card", "mouth", "fire", "rabbit", "books",
+                "crayon", "sticks", "ear", "quarter", "thumb", "blood", "mother", "toothbrush"};
+        this.noun1 = places[number];
+    }
+
+    public String getNoun2()
+    {
+        return noun2;
+    }
+
+    public void setNoun2(String noun2)
+    {
+        this.noun2 = noun2;
+    }
+
+    public void setRandomNoun2()
+    {
+        Random random = new Random();
+        int number = random.nextInt(15);
+        String[] places = {"baseball", "bell", "laborer", "baby", "fish", "screw", "monkey", "weight", "earthquake",
+                "leaf", "theory", "chickens", "riddle", "flowers", "coal"};
+        this.noun2 = places[number];
+    }
+
+    public String getVerbPastTense1()
+    {
+        return verbPastTense1;
+    }
+
+    public void setVerbPastTense1(String verbPastTense1)
+    {
+        this.verbPastTense1 = verbPastTense1;
+    }
+
+    public void setRandomVerbPastTense1()
+    {
+        Random random = new Random();
+        int number = random.nextInt(15);
+        String[] places = {"removed", "solicited", "expanded", "illuminated", "detected", "urged", "marked",
+                "developed", "milked", "whispered", "scowled", "forced", "drilled", "prohibited", "damaged"};
+        this.verbPastTense1 = places[number];
+    }
+
 }
